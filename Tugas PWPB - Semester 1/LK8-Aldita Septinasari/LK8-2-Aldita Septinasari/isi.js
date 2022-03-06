70108ada4d58973c7f57e1e755f4ed9b12fa0ae5
Aldita Septinasari <alditaseptinasari394@gmail.com>
2022-03-06 11:47:22 +0700

    first commit

---
+++ Tugas PWPB - Semester 1/LK8-Aldita Septinasari/LK8-2-Aldita Septinasari/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>")
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("<H1>SMK NEGERI 1 CIAMIS</H1>");
+document.writeln("<H2>TEKNIK KOMPUTER DAN INFORMATIKA</H2>");
+document.writeln("<H3>JURUSAN REKAYASA PERANGKAT LUNAK</H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER>")
